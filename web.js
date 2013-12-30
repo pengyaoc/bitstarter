882cdae762dabd806bc48c46dd08fd7c694dd13f
pengyaoc <pengyaoc@gmail.com>
2013-12-30 19:01:07 +0000

    web js bug fix

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var htmlFile = "index.html";
 var app = express();
 app.use(express.logger());
 
-var buffer = new Buffer(1000);
-buffer = fs.readFileSync('./index.html');
+var buffer = fs.readFileSync(htmlFile);
 
 app.get('/', function(request, response) {
   response.send(buffer.toString("utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
